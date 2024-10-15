1513653293970723990014feec98444e1da93a75
Diego Bravo <diego.art.bra@gmail.com>
2024-10-15 00:35:47 -0300

    +class proyecto
    constructor

---
+++ Sistema de gestion EMP/Clases/Proyecto.py
@@ -0,0 +1,14 @@
+
+
+class proyecto:
+    def _init_(self,id_proyecto,nombre_proyecto,descripcion_proyecto,
+           fecha_inicio,fecha_plazo):
+        self.id_proyecto=id_proyecto
+        self.nombre_proyecto=nombre_proyecto
+        self.descripcio_proyecto=descripcion_proyecto
+        self.fecha_inicio=fecha_inicio
+        self.fecha_plazo=fecha_plazo
+
+
+    def validar_fechas():
+        pass    
