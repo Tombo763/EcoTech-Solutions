01724e0a78baeefd149eddedc2224752691f247a
Tombo763 <olivaresalessandro10@gmail.com>
2024-10-15 23:49:08 -0300

    nombre database en mysql.connector

--- Sistema de gestion EMP/DAL/db.py
+++ Sistema de gestion EMP/DAL/db.py
@@ -4,10 +4,10 @@ cnx = mysql.connector.connect(
     user='root', 
     password='',
     host='127.0.0.1',
-    database='biblioteca')
+    database='ecotech-solutions')
     
 cursor = cnx.cursor()
-cursor.execute('Select * from editorial')
+cursor.execute('Select * from departamentos')
     #cursor.execute("SHOW DATABASES") # Comando usado para testear la conexión, OK
 for base in cursor:
     print(base)
