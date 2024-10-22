29ced2ef037478a5da4a85c2beb0a6ee62f0345b
Diego Bravo <diego.art.bra@gmail.com>
2024-10-22 16:29:33 -0300

    comentario

--- Sistema de gestion EMP/Clases/Tipo_empleado.py
+++ Sistema de gestion EMP/Clases/Tipo_empleado.py
@@ -2,3 +2,7 @@ class tipo_empleado:
     def _init_(self,id_tipo_empleado,tipo_empleado):
         self.id_tipo_empleado=id_tipo_empleado
         self.tipo_empleado=tipo_empleado
+
+
+
+#wena shoro        
